58e81b49d9ef36fffb4f3f042964159fab25c73d
ShengxiangChen <shengxiang.chen@cocos2d-x.org>
2012-07-02 14:21:29 +0800

    typo error

--- Classes/AboutLayer.js
+++ Classes/AboutLayer.js
@@ -11,23 +11,23 @@ var AboutLayer = cc.Layer.extend({
             title.setPosition(cc.ccp(winSize.width / 2, winSize.height - 120));
             this.addChild(title);
 
-            var about = cc.LabelTTF.create("\n \n This showcase utilizes many features from Cocos2d-html5 engine, including: Parallax background, tilemap, actions, ease, frame animation, schedule, Labels, keyboard Dispatcher, Scene Transition. Art and audio is copyrighted by Enigmata Genus Revenge, you may not use any copyrigted material without permission. This showcase is licensed under GPL",cc.SizeMake(winSize.width * 0.85 ,100),cc.TextAlignmentLeft,"Arial",14)
+            var about = cc.LabelTTF.create("\n \n This showcase utilizes many features from Cocos2d-html5 engine, including: Parallax background, tilemap, actions, ease, frame animation, schedule, Labels, keyboard Dispatcher, Scene Transition. Art and audio is copyrighted by Enigmata Genus Revenge, you may not use any copyrigted material without permission. This showcase is licensed under GPL",cc.SizeMake(winSize.width * 0.85 ,100),cc.TEXT_ALIGNMENT_LEFT,"Arial",14)
             about.setPosition(cc.ccp(winSize.width/2,winSize.height/2 + 20));
             this.addChild(about);
 
-            var author = cc.LabelTTF.create("Programmer: \n Shengxiang Chen",cc.SizeMake(winSize.width * 0.85 ,100),cc.TextAlignmentLeft,"Arial",14)
+            var author = cc.LabelTTF.create("Programmer: \n Shengxiang Chen",cc.SizeMake(winSize.width * 0.85 ,100),cc.TEXT_ALIGNMENT_LEFT,"Arial",14)
             author.setPosition(cc.ccp(winSize.width/2,140));
             this.addChild(author);
 
-            author = cc.LabelTTF.create("Dingping Lv",cc.SizeMake(winSize.width * 0.85 ,100),cc.TextAlignmentCenter,"Arial",14)
+            author = cc.LabelTTF.create("Dingping Lv",cc.SizeMake(winSize.width * 0.85 ,100),cc.TEXT_ALIGNMENT_CENTER,"Arial",14)
             author.setPosition(cc.ccp(winSize.width/2,120));
             this.addChild(author);
 
-            author = cc.LabelTTF.create("Effects animation: \n Hao Wu",cc.SizeMake(winSize.width * 0.85 ,100),cc.TextAlignmentLeft,"Arial",14)
+            author = cc.LabelTTF.create("Effects animation: \n Hao Wu",cc.SizeMake(winSize.width * 0.85 ,100),cc.TEXT_ALIGNMENT_LEFT,"Arial",14)
             author.setPosition(cc.ccp(winSize.width/2,100));
             this.addChild(author);
 
-            author = cc.LabelTTF.create("Quality Assurance: \n Sean Lin",cc.SizeMake(winSize.width * 0.85 ,80),cc.TextAlignmentLeft,"Arial",14)
+            author = cc.LabelTTF.create("Quality Assurance: \n Sean Lin",cc.SizeMake(winSize.width * 0.85 ,80),cc.TEXT_ALIGNMENT_LEFT,"Arial",14)
             author.setPosition(cc.ccp(winSize.width/2,80));
             this.addChild(author);
 
