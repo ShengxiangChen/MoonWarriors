c3ff0c7f0b2e52ffba34fb9f455ce063347234d6
ShengxiangChen <shengxiang.chen@cocos2d-x.org>
2012-08-16 16:01:59 +0800

    fff

--- MoonWarriors/MoonWarriors-html5.js
+++ MoonWarriors/MoonWarriors-html5.js
@@ -37,7 +37,7 @@ var MW = MW || {};
         showFPS:true,
         frameRate:60,
         tag:'gameCanvas', //the dom element to run cocos2d on
-        engineDir:'libs/cocos2d/',
+        engineDir:'http://192.168.0.122:8080/cocos2d/',
         appFiles:[
             'MoonWarriors/src/Resource.js',
             'MoonWarriors/src/config/GameConfig.js',
